53eecf1764f56aa31a5c576a3af56036be55ca79
jameslawford6725-dev <jameslawford6725@gmail.com>
2026-02-17 15:08:15 +0800

    Create App.js for Space Roller Casino

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Space Roller Casino</h1>
+            <p>Experience the thrill of gaming in space!</p>
+        </div>
+    );
+};
+
+export default App;
